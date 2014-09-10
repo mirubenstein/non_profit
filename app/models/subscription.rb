731539cc4ade2821540a9f5627f3be2ce959f462
moof mayeda & mark rubenstein <student@epicodus.com>
2014-09-10 16:50:48 -0700

    create new subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,5 +3,18 @@ class Subscription < ActiveRecord::Base
   validates :amount, presence: true
   validates :interval, presence: true
   validates :long_name, presence: true
-  validates :short_name, presence: true
+  validates :short_name, presence: true, uniqueness: true
+  before_create :create_plan
+
+private
+  def create_plan
+    Stripe.api_key = "sk_test_w3CCvv0csnbJams8XeOA9xKE"
+    Stripe::Plan.create(
+      :amount => 2000,
+      :interval => self.interval,
+      :name => self.long_name,
+      :currency => self.currency,
+      :id => self.short_name
+    )
+  end
 end
